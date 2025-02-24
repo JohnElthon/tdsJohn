6737ffc8e6cceebb9e1badb7e4673a06b47397c6
JohnElthon <johnelthon1@gmail.com>
2025-02-24 11:54:39 -0300

    aula nova

---
+++ JavaScriptAula1/AtividadeJavascriptEx1/ex1.js
@@ -0,0 +1,24 @@
+function VerificarVelocidade(){
+let velocidade = window.document.getElementById('velocidade');
+let valor = Number(velocidade.value);
+let resultado = window.document.getElementById('resultado');
+
+if (valor > 40 && valor < 100){
+    console.log('Multado!')
+    resultado.innerHTML = '<strong>Multado</strong>';
+}
+else if (valor > 100){
+    console.log('Multado e Veiculo será apreendido!')
+    resultado.innerHTML = '<strong>Multado e Veiculo será apreendido!</strong>';
+}
+else {
+    console.log('Dentro da velocidade permitida!')
+    resultado.innerHTML = '<strong>Dentro da velocidade permitida</strong>'
+}
+
+
+
+
+
+
+}
