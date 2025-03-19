9fd261902169744e51b682533c57db496cbc06f3
JohnElthon <johnelthon1@gmail.com>
2025-03-19 11:37:45 -0300

    .

---
+++ JavaScriptAula1/ExFinalJavaScript/exfinal.js
@@ -0,0 +1,14 @@
+function Calcular(){
+    let Valor = document.getElementById('ValorRef');
+    let ValorRef = parseFloat(Valor.Value)
+    let Gasolina = document.getElementById('Gasolina');
+    let Etanol = document.getElementById('Etanol');
+    let Porcentagem = document.getElementById('Porcentagem');
+    let Resultado = document.getElementById('Resultado');
+
+   if ( 
+   )
+
+
+
+}
