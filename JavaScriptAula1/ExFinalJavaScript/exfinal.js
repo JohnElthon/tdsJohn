ca61e7d6726e97001f32b643b69f727806b8d34e
JohnElthon <johnelthon1@gmail.com>
2025-03-26 11:08:16 -0300

    .

--- JavaScriptAula1/ExFinalJavaScript/exfinal.js
+++ JavaScriptAula1/ExFinalJavaScript/exfinal.js
@@ -1,14 +1,33 @@
-function Calcular(){
-    let Valor = document.getElementById('ValorRef');
-    let ValorRef = parseFloat(Valor.Value)
-    let Gasolina = document.getElementById('Gasolina');
-    let Etanol = document.getElementById('Etanol');
-    let Porcentagem = document.getElementById('Porcentagem');
-    let Resultado = document.getElementById('Resultado');
+function calcular(){
+     
+  let preco = document.getElementById('preco');
+  let valor = Number(preco.value);
 
-   if ( 
-   )
+  let select = document.getElementById('select');
 
+  let valores = select.options [select.selectedIndex];
+ 
+  let porcetagem = valores.value;
 
+  let calcgasolina = (valor*porcetagem);
+  // esse código é outro jeito de pegar duas casas de  números
+  const total = calcgasolina.toFixed(2)
+  // conta para calcular o valor do etanol
+  let calcetanol = valor* (1/porcetagem);
+  let resultado = document.getElementById('resultado');
+   
+  const totalgasolina = calcgasolina.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  const totaletanol = calcetanol.toLocaleString('pt-BR' ,{style: 'currency' , currency: 'BRL' } );
 
+  if (document.getElementById('gasolina').checked){
+    resultado.innerText = `Abasteça com Etanol se o seu  preço for menor  ou igual a : ${totalgasolina}`;
+  }
+
+  else if (document.getElementById('etanol').checked){
+      resultado.innerText = `Abasteça com gasolina se o seu o seu preço for menor ou igual a :  ${totaletanol}`;
+  }
+
+  else{
+      resultado.innerText = "Nenhum valor encontrado";
+  }
 }
